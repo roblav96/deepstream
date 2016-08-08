d63b4518bb754517316e9a3f6ebc2d77687f155d
roblav96 <roblav96@gmx.com>
2016-08-07 21:06:02 -0400

    checkin it

--- server/deepstream.js
+++ server/deepstream.js
@@ -1,30 +1,18 @@
 // 
 
-const DeepstreamServer = require( 'deepstream.io' )
-const C = DeepstreamServer.constants
-	/*
-	The server can take
-	1) a configuration file path
-	2) null to explicitly use defaults to be overriden by server.set()
-	3) left empty to load the base configuration from the config file located within the conf directory.
-	4) pass some options, missing options will be merged with the base configuration
-	*/
-const server = new DeepstreamServer( {
+var DeepstreamServer = require( 'deepstream.io' )
+var RethinkDBStorageConnector = require( 'deepstream.io-storage-rethinkdb' )
+
+var server = new DeepstreamServer( {
 	host: 'localhost',
 	port: 6020,
 	tcpPort: 6021
 } )
 
-/*
-Optionally you can specify settings to set in complex objects, such as dataTransforms, a HTTPServer
-or a plugin which you want to reuse within your application
-*/
-server.set( 'dataTransforms', [ {
-	topic: C.TOPIC.RPC,
-	action: C.ACTIONS.REQUEST,
-	transform: function ( data, metaData ) {}
-} ] )
+server.set( 'storage', new RethinkDBStorageConnector( {
+	port: 5672,
+	host: 'localhost'
+} ) )
 
-// start the server
 server.start()
 
